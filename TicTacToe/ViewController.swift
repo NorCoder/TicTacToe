d91d7e904fc01bea745612baa20551e08c9d46e1
Kjetil Korsveien <korsveien@me.com>
2017-01-28 19:33:20 +0100

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Kjetil Korsveien on 28.01.2017.
+//  Copyright © 2017 Kjetil Korsveien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
